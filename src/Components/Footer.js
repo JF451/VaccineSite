8faee9711a0758224ffdc6b231e223e0ba98f78e
Justin Fulkerson <justinfulkerson@Justins-MacBook-Pro.local>
2021-08-22 19:19:25 -0700

    Added to Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import './footer.css';
+import './footer.css'
 
+function Footer()
+{
 
-function Footer() {
     return (
-        <div>
-            <h1>This is a footer</h1>
+        <div className="Footer">
+            <div className="container">
+                <div className="row">
+
+                    <div className="col">
+                        <h4 className="block ">About Us</h4>
+                    </div>
+
+                    <div className="col">
+                        <h4 className="block">Contact</h4>
+                    </div>
+
+                    <div className="col">
+                        <h4 className="block"><a className="block" href={"https://www.cdc.gov/coronavirus/2019-ncov/faq.html"}>COVID-19 FAQ</a></h4>
+                    </div>
+
+                    <div className="col">
+                        <h4 className="block"><a className="block" href={"https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/quarantine.html"}>CDC Guidelines</a></h4>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
