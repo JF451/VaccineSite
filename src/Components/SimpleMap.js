1e1106f1d2080abf3459c1a1a2128ff7e26f904d
Justin Fulkerson <justinfulkerson@Justins-MacBook-Pro.local>
2021-08-28 23:22:44 -0700

    Need to add conent next to map

--- src/Components/SimpleMap.js
+++ src/Components/SimpleMap.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
+import { DropdownButton, Dropdown } from 'react-bootstrap';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -14,19 +15,31 @@ class SimpleMap extends Component {
 
     render() {
         return(
-            <div style={{ height: '50vh', width: '50%' }}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: 'AIzaSyBcNjjKbKT4Fror-w5C8Ch7GoMaEq_YXAE' }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                >
-                    <AnyReactComponent
-                        lat={33.9806}
-                        lng={117.3755}
-                        text="My Marker"
-                    />
-                    </GoogleMapReact>
-            </div>
+            <div className="parent-map">
+
+                <div style={{ height: '50vh', width: '75%' }} className="map-display" >
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: 'AIzaSyBcNjjKbKT4Fror-w5C8Ch7GoMaEq_YXAE' }}
+                        defaultCenter={this.props.center}
+                        defaultZoom={this.props.zoom}
+                    >
+                        <AnyReactComponent
+                            lat={33.9806}
+                            lng={-117.3755}
+                            text="My Marker"
+                        />
+                        </GoogleMapReact>
+
+                        <div className="map-data">
+                            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                                <Dropdown.Item href="#/action-1">5 miles</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">10 miles</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">25 miles</Dropdown.Item>
+                            </DropdownButton>
+
+                        </div>
+                </div>
+        </div>
         )
     }
 }
